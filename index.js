59fd8cb80ea135b16cf6215a8579c9a79efd7c5f
Victoria <victoriathorsenn@gmail.com>
2021-06-03 22:06:25 -0400

    commit tree b0fcdd6cde5c6605e13255bdfab51431df2f35c9

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 
+const superbowlWin = (array) => {
+    let win = array.find(array => array.result === "W")
+    if (!!win) {
+        return win.year
+    } else {
+        return undefined
+    }
+        
+
+}
